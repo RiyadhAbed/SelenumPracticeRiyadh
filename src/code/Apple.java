85449ade120ad7e60f48ffa2426649e1fa468fcb
RiyadhAbed <riyadh.abed@tekschool.us>
2022-07-23 19:25:57 -0400

    1

--- src/code/Apple.java
+++ src/code/Apple.java
@@ -1,9 +1,23 @@
 package code;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Apple {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.get("https://www.google.com/");
+		JavascriptExecutor jas = (JavascriptExecutor)driver;
+		jas.executeScript("decument.getElementById('User').value = 'SoftwareTestingMaterial.com");
+		
 
 	}
 
