af2842a13578cb4486bd82131d997807cfc4f16b
RiyadhAbed <riyadh.abed@tekschool.us>
2022-07-27 12:53:52 -0400

    new class

--- src/code/Apple.java
+++ src/code/Apple.java
@@ -2,8 +2,11 @@ package code;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Apple {
@@ -15,8 +18,16 @@ public class Apple {
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		driver.get("https://www.google.com/");
-		JavascriptExecutor jas = (JavascriptExecutor)driver;
-		jas.executeScript("decument.getElementById('User').value = 'SoftwareTestingMaterial.com");
+		//JavascriptExecutor jas = (JavascriptExecutor)driver;
+		//jas.executeScript("decument.getElementById('User').value = 'SoftwareTestingMaterial.com");
+		driver.findElement(By.xpath("//a[@class='MV3Tnb']")).click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.close();
 		
 
 	}
